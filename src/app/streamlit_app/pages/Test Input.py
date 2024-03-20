e0ac59263b9cddb4e02fa0a5f4e9a824f8901629
ravi <ravi@iwell.nl>
2024-03-21 00:44:10 +0100

    correct cats

--- src/app/streamlit_app/pages/Test Input.py
+++ src/app/streamlit_app/pages/Test Input.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from app.data.competencies import get_compentencies_for_subject_code
 
 def main():
     st.title("Test Category Input")
 
     question = st.text_input("Enter a question")
-    dropdown = st.selectbox("Select a test category", [' ','addition', 'subtraction', 'multiplication'])
+
+    categories = get_compentencies_for_subject_code("MA")["bezeichnung"].unique()
+    dropdown = st.selectbox("Select a test category",
+                            [descr for descr in categories])
 
     # Display the test categories
     if dropdown != ' ':
