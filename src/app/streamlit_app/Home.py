9ca09ca055dc86443a5a186664be5a4301d75e8c
calwinship <winship.cal@gmail.com>
2024-03-21 21:21:27 +0100

    home page improve

--- src/app/streamlit_app/Home.py
+++ src/app/streamlit_app/Home.py
@@ -1,13 +1,23 @@
 import streamlit as st
 
-st.image('src/app/streamlit_app/logos/logo_trans.png')
+st.sidebar.image('src/app/streamlit_app/logos/logo_trans.png')
 
-st.write(''' \n\n\n\n\n
-            
-            
-            DRUID is an Primary School Teacher platform \n\nNow. Every student's progress can be monitored to help them fulfil their potential.
-        
-        '''
-            
-            )
+st.markdown("<h1 style='text-align: center; font-family: Alexandria; font-size: 100px ;color: #27b1b1;'>DRUID</h1>", unsafe_allow_html=True)
 
+st.write('''DRUID is a revolutionary Primary School Teacher platform \n\nNow. Every student's progress can be monitored to help them fulfil their potential. \n\n With in-depth knowledge of the curriculum and student data, DRUID can make personalised teaching recommendations.''')
+
+
+if st.button('Instructions', type="primary"):
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.subheader("1. Upload your test")
+        st.image('src/app/streamlit_app/images/upload.jpg')
+
+    with col2:
+        st.subheader("2. Grade your test")
+        st.image('src/app/streamlit_app/images/teacher.jpg')
+
+    with col3:
+        st.subheader("3. Track with AI")
+        st.image('src/app/streamlit_app/images/graph.png')
