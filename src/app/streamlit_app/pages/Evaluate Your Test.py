e4bcc8aacf31da2d782171a1bc13704c2526807a
Jakub Janicki <jjanicki08@gmail.com>
2024-03-21 13:32:20 +0100

    Grade Display Done

--- src/app/streamlit_app/pages/Evaluate Your Test.py
+++ src/app/streamlit_app/pages/Evaluate Your Test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_modal import Modal
 
-from app.streamlit_app.database import setup_database, get_students, add_to_students, add_to_evaluation_types, get_evaluation_types, add_to_tests, get_tests, add_test_evaluation_to_test, get_test_evaluations, get_all_test_evaluations, add_student_to_tests, add_test_evaluation_to_student, get_student_tests, get_student_test_evaluations, get_all_student_test_evaluations, get_test_students, seed_database
+from app.streamlit_app.database import *
 
 st.set_page_config(
     layout="wide",
@@ -12,9 +12,13 @@ st.title("Test Grading")
 
 
 class Test:
-    def __init__(self, name, evaluations):
+    def __init__(self, id, name, evaluations):
+        self.id = id
         self.name = name
         self.evaluations = evaluations
+
+    def get_id(self):
+        return self.id
 
     def get_name(self):
         return self.name
@@ -23,29 +27,32 @@ class Test:
         return self.evaluations
 
 
-def find_mark(row):
-    if row[2]:
-        return 1
-    if row[3]:
-        return 2
-    if row[4]:
-        return 3
-    if row[5]:
-        return 4
+tests = get_tests()
 
-students = get_students()
+Tests = []
 
-Test1 = Test("Test1", ["können Bilder wahrnehmen, beobachten und darüber reflektieren", "können Bilder wahrnehmen, beobachten und darüber reflektieren."])
-Test2 = Test("Test2", ["Why", "Where", "When"])
-
-Tests = [Test1, Test2]
+for test_row in tests:
+    test_competencies = []
+    for c in get_test_competencies(test_row.id):
+        test_competencies.append(c.get_competency_type().type)
+    Tests.append(Test(test_row.id, test_row.test_name, test_competencies))
 
 test_evaluations = {test.get_name(): test.get_evaluations() for test in Tests}
+
 
 # Use the select box widget
 option = st.selectbox(
     'Select Test for Grading',
     test_evaluations.keys())
+
+current_test = tests[0].id
+
+for test in Tests:
+    if test.get_name() == option:
+        current_test = test.id
+
+
+students = get_test_students(current_test)
 
 # Create Student Marks Array
 student_marks = []
@@ -67,32 +74,45 @@ for i, student in enumerate(students):
     edited_df = st.data_editor(df, use_container_width=True, disabled="col1", hide_index="true", height=None, key=i)
     student_marks.append(edited_df)
 
-# print(edited_df.keys())
+
+
+
+
+def find_mark(row):
+    if row[2]:
+        return 1
+    if row[3]:
+        return 2
+    if row[4]:
+        return 3
+    if row[5]:
+        return 4
 
 for data in student_marks:
     for idx, row in enumerate(data.itertuples(), start=1):
         st.markdown(f"{find_mark(row)}")
 
 
-def saveData():
-    print(students[1].id)
-    modal.close()
 
 
-st.button("Save Changes", on_click=saveData, type="primary")
-
-modal = Modal(
-    "Do you want to confirm changes?",
-    key="confirm modal",
-)
-
-open_modal = st.button("Open", on_click=modal.open)
-
-
-if modal.is_open():
-    with modal.container():
-        col1, col2, col3 = st.columns([6, 1, 1])
-        with col2:
-            st.button('Confirm')
-        with col3:
-            st.button('Cancel', on_click=saveData)
+# Modal Stuff
+# def saveData():
+#     print(get_test_competencies(4)[0].get_competency_type().type)
+#     modal.close()
+#
+#
+# modal = Modal(
+#     "Do you want to confirm changes?",
+#     key="confirm modal",
+# )
+#
+# st.button("Save Changes", on_click=modal.open, type="primary")
+#
+#
+# if modal.is_open():
+#     with modal.container():
+#         col1, col2, col3 = st.columns([6, 1, 1])
+#         with col2:
+#             st.button('Confirm')
+#         with col3:
+#             st.button('Cancel', on_click=saveData)
